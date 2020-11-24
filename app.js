28f72cb29be85ba4dede76d6a8bcbe8c6de59ec8
Roland Gjoka <rolandgjoka@hotmail.com>
2020-11-23 22:04:50 -0600

    output HTML file

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-co
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const inquirer = require("inquirer")
+const jest = require('jest')
